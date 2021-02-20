b1ed4dc747d981a324f1c237999fdd9037acc470
tomityrrell <tomi.tyrrell@icloud.com>
2021-02-20 01:44:14 -0500

    Comments!
    Made a few notes and updated calls to chivaxbot

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from datetime import datetime
 import pytz
@@ -11,7 +12,10 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-import chivaxbot
+import chivaxbot as chivaxbot
+#import chivaxbot
+
+retries = 5
 
 # Get current date and create directory for new files
 now = datetime.now(pytz.timezone('America/New_York'))
@@ -33,28 +37,54 @@ state_pop = county_pops.sum()
 # covid = pd.DataFrame(res.text)
 
 
-def scraper(mode="Firefox"):
+def get_driver(mode="Firefox"):
     options = Options()
     options.headless = True
     if mode == "Firefox":
-        driver = webdriver.Firefox(options=options, executable_path='geckodriver')
+        driver = webdriver.Firefox(options=options, executable_path='./geckodriver')
     elif mode == "Remote":
         driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub", options=options)
     driver.implicitly_wait(5)
 
+    return driver
+
+
+def scraper(mode="Firefox"):
+    driver = get_driver(mode)
+
+    # TODO Create single scrapping method for case and vaccine data
+
+    # DATA SOURCE #1
+    #
     # Pull case data from NJ Covid Dashboard at https://njhealth.maps.arcgis.com/apps/opsdashboard/index.html#/795729487af44255a1357c73fd8d8b4e
+    #
+
+    # URL
     case_data_url = "https://njhealth.maps.arcgis.com/apps/opsdashboard/index.html#/795729487af44255a1357c73fd8d8b4e"
     driver.get(case_data_url)
 
+    # XPATH
     # Scrape case data by county
     case_data_xpath = "//span[contains(@id,'ember') and contains(@class,'flex') and @style='']"
     case_data_elements = driver.find_elements(By.XPATH, case_data_xpath)
+    while not case_data_elements and retries > 0:
+        print("Case data elements not found.  {} attempt(s) left.  Retrying in 5 seconds...".format(retries))
+        time.sleep(5)
+        retries = retries - 1
+        case_data_elements = driver.find_elements(By.XPATH, case_data_xpath)
 
+    # DATA PROCESSING
     county_names = list(map(lambda e: e.find_elements_by_tag_name("p")[0].text.replace(" County", "").replace(" ", "_").lower(), case_data_elements))
     cases_field_names = list(filter(lambda s: s != "", map(lambda t: t.text[t.text.index(" "):].strip().replace("Total ", ""), case_data_elements[0].find_elements_by_tag_name("td"))))
     cases_data = list(map(lambda t: list(filter(lambda s: s != "", map(lambda t: t.text.replace(",", "").strip().split(" ")[0], t.find_elements_by_tag_name("td")))), case_data_elements))
+    cases_df = pd.DataFrame(cases_data, index=county_names, columns=cases_field_names)
 
+    # DATA SOURCE # 2
+    #
     # Pull vaccine data from NJ Covid Dashboard at https://njhealth.maps.arcgis.com/apps/opsdashboard/index.html#/c99909df3f994c2ab07134f9f746000c
+    #
+
+    # URL
     vaccine_data_url = "https://njhealth.maps.arcgis.com/apps/opsdashboard/index.html#/c99909df3f994c2ab07134f9f746000c"
     driver.get(vaccine_data_url)
 
@@ -64,20 +94,25 @@ def scraper(mode="Firefox"):
     # vaccination_tab_xpath = "//button[contains(text(),'Vaccination Overview')]"
     # driver.find_element_by_xpath(vaccination_tab_xpath).click()
 
+    # XPATH
     # Scrape vaccine data by county
     vaccine_data_xpath = "//span[contains(@id,'ember') and contains(@class,'flex')]"
     vaccine_data_elements = driver.find_elements(By.XPATH, vaccine_data_xpath)
+    while not vaccine_data_elements and retries > 0:
+        print("Vaccine data elements not found.  {} attempt(s) left.  Retrying in 5 seconds...".format(retries))
+        retries = retries - 1
+        time.sleep(5)
+        case_data_elements = driver.find_elements(By.XPATH, case_data_xpath)
+
+    # DATA PROCESSING
     vaccine_data = list(map(lambda s: s.text.replace(",","").replace(" COUNTY", "").replace(" Doses Administered", "").replace(" ", "_").lower().split("\n"), vaccine_data_elements))
-
-    driver.quit()
-
-    # Tie it up with a DataFrame and a bow on top
-    cases_df = pd.DataFrame(cases_data, index=county_names, columns=cases_field_names)
-
     vaccine_df = pd.DataFrame(vaccine_data, columns=["county", "Vaccine Doses"])
     vaccine_df.index = vaccine_df.county
     vaccine_df.drop("county", axis=1, inplace=True)
 
+    driver.quit()
+
+    # COMBINE DATA SOURCES
     # Merge cases and vaccine data into one DF
     covid_df = cases_df.merge(vaccine_df, left_index=True, right_index=True).astype("int")
     covid_df.sort_index(inplace=True)
@@ -86,6 +121,7 @@ def scraper(mode="Firefox"):
 
     covid_df.to_csv("./data/nj_covid_{}.csv".format(now.date()), index_label="County")
 
+    # DATA NORMALIZATION
     # Normalize df and create discrepancy columns
     scaler = StandardScaler().fit(covid_df)
     ndf = pd.DataFrame(scaler.transform(covid_df))
@@ -100,14 +136,20 @@ def scraper(mode="Firefox"):
 
 
 def imager(data_dict, palette, name):
-    colors = chivaxbot.get_colors_dict(data_dict, palette, "vax")
-    chivaxbot.write_svg("./images/New_Jersey_Counties_Outline.svg", "./images/{}/{}_{}.png".format(now.date(), name, now.date()), colors)
+    colors = chivaxbot.get_colors_dict(data_dict, palette, name)
+    chivaxbot.write_svg("./images/New_Jersey_Counties_Outline.svg", ["./images/{}/{}_{}.png".format(now.date(), name, now.date())], colors)
 
     print("{} imaging successful!".format(name))
 
 
 if __name__ == '__main__':
-    df, ndf = scraper(mode="Remote")
+    df, ndf = scraper(mode="Firefox")
+
+    # Percentages inter-county
+    df_by_county = df.divide(df.Population, axis=0)
+
+    # Percentages intra-county/state level
+    df_by_category = df/df.sum()
 
     imager(df["Vaccine Doses"].to_dict(), chivaxbot.vax_colorscale, "vaccines")
     imager(df["Confirmed Cases"].to_dict(), chivaxbot.deaths_colorscale, "cases"),
@@ -116,3 +158,5 @@ if __name__ == '__main__':
     disc_colorscale = ["#641e16", " #922b21", " #c0392b", " #e6b0aa ", " #f9ebea"]
     imager(ndf["Vaccine-Case Discrepancy"].to_dict(), disc_colorscale, "case-vax-disc")
     imager(ndf["Vaccine-Death Discrepancy"].to_dict(), disc_colorscale, "death-vax-disc")
+
+    imager(df_by_county["Confirmed Deaths"].to_dict(), disc_colorscale, "deaths_by_county")
